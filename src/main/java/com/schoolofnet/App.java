85e19c4f625662c17ad7a6587a55d9f6980997f4
Luiz Felipe Moraes <lffm1994@gmail.com>
2022-01-19 17:44:54 -0300

    Java lang - Object

--- src/main/java/com/schoolofnet/App.java
+++ src/main/java/com/schoolofnet/App.java
@@ -1,15 +1,19 @@
 package com.schoolofnet;
 
-import com.schoolofnet.private_code.Private;
-import com.schoolofnet.protected_code.Protected;
 import com.schoolofnet.public_code.Public;
 
 public class App {
     public static void main(String[] args) {
-        new Public();
-        new Private();
-        new Protected();
 
-        System.out.println( "Hello World!" );
+        // Classe Object
+        // Qualquer tipo de objeto vem sobrescrito de Object ela e a raiz de hierarquia das classes
+        // Ela sempre vai ser usada diretamente ou indiretamente.
+        Object obj = new Object();
+        Public publicCode = new Public();
+
+        System.out.println(obj.toString());
+        System.out.println(publicCode.toString());
+
+        System.out.println("Hello World!");
     }
 }
--- src/main/java/com/schoolofnet/public_code/Public.java
+++ src/main/java/com/schoolofnet/public_code/Public.java
@@ -1,8 +1,15 @@
 package com.schoolofnet.public_code;
 
 public class Public {
+
+
     // O acesso e liberado para qualquer lugar do software.
     public Public() {
         System.out.println("Public class");
+    }
+
+    @Override
+    public String toString() {
+        return "Public class toString()";
     }
 }
