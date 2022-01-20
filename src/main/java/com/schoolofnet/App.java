8d44571e039ac83da4c56c650a3a755afeb5c37f
Luiz Felipe Moraes <lffm1994@gmail.com>
2022-01-20 11:33:34 -0300

    Java lang - Classes Wrappers

--- src/main/java/com/schoolofnet/App.java
+++ src/main/java/com/schoolofnet/App.java
@@ -5,14 +5,29 @@ import com.schoolofnet.public_code.Public;
 public class App {
     public static void main(String[] args) {
 
-        // Classe Object
-        // Qualquer tipo de objeto vem sobrescrito de Object ela e a raiz de hierarquia das classes
-        // Ela sempre vai ser usada diretamente ou indiretamente.
-        Object obj = new Object();
-        Public publicCode = new Public();
+        // Classes Wrappers
+        // Classes que tem a herança de Object
+        // Existência dos métodos de Object.
 
-        System.out.println(obj.toString());
-        System.out.println(publicCode.toString());
+//      |  Tipo primitivo  | Tipos Wrappers |
+//      |  int             | Integer        |
+//      |  boolean         | Boolean        |
+//      |  double          | Double         |
+//      |  float           | Float          |
+//      |  long            | Long           |
+        
+        Integer one = 0;
+
+        // existencia dos metodos objects
+
+        // Classe Object
+        // Qualquer tipo de /objeto vem sobrescrito de Object ela e a raiz de hierarquia das classes
+        // Ela sempre vai ser usada diretamente ou indiretamente.
+//        Object obj = new Object();
+//        Public publicCode = new Public();
+//
+//        System.out.println(obj.toString());
+//        System.out.println(publicCode.toString());
 
         System.out.println("Hello World!");
     }
